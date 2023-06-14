ef5a50b71674048d9926eec913142d2197ec7370
hannaax <hanan941008@gmail.com>
2023-06-14 11:27:56 +0900

    section06 container-presenter

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+// import "../styles/globals.css";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client"
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component }) {
+  const client = new ApolloClient({
+    uri: "http://backend-example.codebootcamp.co.kr/graphql",
+    cache: new InMemoryCache(), // 컴퓨터의 메모리에다가 백엔드에서 받아온 데이터 임시로 저장 => 나중에 더 자세히
+  })
+
+  return (
+    <div>
+      <div>=======여기는 _app.js 컴포넌트 입니다.========</div>
+      <ApolloProvider client={client}>
+        <Component />
+      </ApolloProvider>
+      <div>=======여기는 _app.js 컴포넌트 입니다.========</div>
+    </div>
+  )
 }
 
 export default MyApp
---
+++ pages/section06/06-01-container-presenter/index.js
@@ -0,0 +1,6 @@
+import BoardWrite from "../../../src/components/units/board/write/BoardWrite.container"
+
+export default function GraphqlMutationPage() {
+  // 한줄일때 괄호() 필요 없음
+  return <BoardWrite />
+}
